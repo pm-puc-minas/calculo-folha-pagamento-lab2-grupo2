50a19fcfbb181ab9676cbb2b7dca2f0601d82a8f
Rafael Lopes de Souza da Fonseca <rafaellopessouzafonseca@gmail.com>
2025-09-25 18:03:14 -0300

    atualizandoEsqueleto

---
+++ src/main/java/com/rh/folhaPagamento/model/Adicionais.java
@@ -0,0 +1,37 @@
+public class Adicionais {
+    private double horasExtras;
+    private double adicionalNoturno;
+    private double comissao;
+
+    public calcularHorasExtras(){
+
+    }
+    public calcularAdicionalNoturno(){
+
+    }
+    public calcularComissao(){
+
+    }
+
+    public double getHorasExtras() {
+        return horasExtras;
+    }
+
+    public double getAdicionalNoturno() {
+        return adicionalNoturno;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setHorasExtras(double horasExtras) {
+        this.horasExtras = horasExtras;
+    }
+    public void setAdicionalNoturno(double adicionalNoturno) {
+        this.adicionalNoturno = adicionalNoturno;
+    }
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+}
---
+++ src/main/java/com/rh/folhaPagamento/model/Funcionario.java
@@ -0,0 +1,14 @@
+
+public class Funcionario
+{
+    private String nome;
+    private String cpf;
+}
+public class Funcionario
+{
+    private String nome;
+    private String cpf;
+    private String cargo;
+    private double salarioBase;
+    private double horasTrabalhadas;
+}
