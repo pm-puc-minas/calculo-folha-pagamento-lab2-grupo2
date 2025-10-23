f7722a6281104d738e8a500c0ddbe9867b161dd0
caiokfuri <caioakfuri@gmail.com>
2025-10-23 18:20:01 -0300

    Consertando chamada de api pelo front

--- src/main/frontend/src/services/api.js
+++ src/main/frontend/src/services/api.js
@@ -5,4 +5,3 @@ const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 export const api = axios.create({
   baseURL: API_URL
 });
-
--- src/main/java/com/rh/folhaPagamento/config/WebConfig.java
+++ src/main/java/com/rh/folhaPagamento/config/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Aplica a todos os endpoints que começam com /api/ (Mudar isso, por enquanto deixar assim)
-                .allowedOrigins("http://localhost:5173")
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
